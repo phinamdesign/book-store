4a2787cb46004ecfc05ad519761ec34ef26c21c2
Phi Nam <phinamhd1992@gmail.com>
2020-04-07 21:38:17 +0700

    update bookcontroller

--- src/main/java/com/vsii/bookstore/controller/BookController.java
+++ src/main/java/com/vsii/bookstore/controller/BookController.java
@@ -1,10 +1,12 @@
 package com.vsii.bookstore.controller;
 
 import com.vsii.bookstore.model.Book;
+import com.vsii.bookstore.security.jwt.JwtProvider;
 import com.vsii.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,11 +17,17 @@ import static com.vsii.bookstore.model.Write.writeExcel;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/auth/books")
 public class BookController {
 
     @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
     private BookService bookService;
+
+    @Autowired
+    JwtProvider jwtProvider;
 
     @GetMapping
     public ResponseEntity<?> getListBook() throws IOException {
@@ -34,7 +42,7 @@ public class BookController {
     public ResponseEntity<?> createBook(@Valid @RequestBody Book book) throws IOException {
         bookService.save(book);
         List<Book> books=(List<Book>) bookService.findAll();
-        final String excelFilePath = "D:\\VSI\\demo/books.xlsx";
+        final String excelFilePath = "D:\\VSI\\jwt/books.xlsx";
         writeExcel(books, excelFilePath);
         return new ResponseEntity<>(book,HttpStatus.CREATED);
     }
